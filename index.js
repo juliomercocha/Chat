8ae16970c4e6a88dc4508f5c822291342a6bf4ab
Augusto Barco <augustopayza@gmail.com>
2022-02-10 13:15:04 -0400

    feat: New styles and improvements

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 
 //setting
 
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-// start the server 
+// start the server
 
-const server = app.listen(app.get('port'), () => {
-    console.log('server on port', app.get('port'));
+const server = app.listen(app.get("port"), () => {
+  console.log("server on port", app.get("port"));
 });
 
 // static files
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const SocketIO = require('socket.io');
+const SocketIO = require("socket.io");
 const io = SocketIO(server);
 
 // websockets
 
-io.on('connection', (socket) => {
-    console.log('new connection', socket.id);
+io.on("connection", (socket) => {
+  console.log("new connection", socket.id);
 
-    socket.on('chat:message', (data) => {
-        io.sockets.emit('chat:message', data)
-    })
-    socket.on('chat:typing', (data) => {
-        socket.broadcast.emit('chat:typing',data);
-    })
+  socket.on("chat:message", (data) => {
+    console.log({ data });
+    io.sockets.emit("chat:message", data);
+  });
+  socket.on("chat:typing", (data) => {
+    socket.broadcast.emit("chat:typing", data);
+  });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,30 +1,54 @@
-const socket = io()
+const socket = io();
 
 // DOM elements
-let message = document.getElementById('message');
-let username = document.getElementById('username');
-let btn = document.getElementById('send');
-let output = document.getElementById('output');
-let actions = document.getElementById('actions');
+let message = document.getElementById("message");
+let username = document.getElementById("username");
+let btn = document.getElementById("send");
+let output = document.getElementById("output");
+let actions = document.getElementById("actions");
 
-btn.addEventListener('click', function() {
-    socket.emit('chat:message', {
-        message: message.value,
-        username: username.value
-    });
+btn.addEventListener("click", function () {
+  socket.emit("chat:message", {
+    message: message.value,
+    username: username.value,
+  });
+
+  // clear message
+  message.value = "";
 });
 
-message.addEventListener('keypress',function() {
-    console.log(username.value);
-    socket.emit('chat:typing',username.value);
+message.addEventListener("keypress", function () {
+  console.log(username.value);
+  socket.emit("chat:typing", username.value);
 });
 
-socket.on('chat:message', function(data) {
-    output.innerHTML += `<p>
-    <strong>${data.username}</strong>: ${data.message}
-    </p>`
+socket.on("chat:message", function (data) {
+  if (data.username === username.value) {
+    output.innerHTML += `<section class="message -right">
+                            <!-- Balloon -->
+                            <div class="nes-balloon from-right">
+                              <p><strong>${data.username}</strong>: ${data.message}</p>
+                            </div>
+                            <i class="nes-bulbasaur"></i>
+                          </section>
+   `;
+  } else {
+    output.innerHTML += `<section class="message -left">
+                            <i class="nes-mario"></i>
+                            <!-- Balloon -->
+                            <div class="nes-balloon from-left">
+                                <p>
+                                    <strong>${data.username}</strong>: ${data.message}
+                                </p>
+                            </div>
+                        </section>
+   `;
+
+    // clean action
+    actions.innerHTML = "";
+  }
 });
 
-socket.on('chat:typing', function(data) {
-    actions.innerHTML = `<p><em>${data} is typing messague </em></p>`
+socket.on("chat:typing", function (data) {
+  actions.innerHTML = `<p><em>${data} is typing messague </em></p>`;
 });
